6d3087ab833a3b274b8f38a041e874bdcff87b03
Brian Simon <bsssshhhhhhh@gmail.com>
2022-07-23 21:43:25 -0400

    buttons

--- src/AbvInput.vue
+++ src/AbvInput.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>
+  <div class="abv-input">
     <input
       type="number"
       step="0.001"
       v-model="value" />
+    <button @click="value -= 0.01">-0.01</button>
+    <button @click="value += 0.01">+0.01</button>
   </div>
 </template>
 
@@ -31,8 +33,21 @@ export default defineComponent({
 </script>
 
 <style scoped>
+.abv-input {
+  display: flex;
+  gap: 1rem;
+}
+
 input {
   padding: 0.25rem 0.5rem;
   font-size: 1.25rem;
+}
+
+button {
+  border: none;
+  background: transparent;
+  box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.5);
+
+  flex-grow: 1;
 }
 </style>
