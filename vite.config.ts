277b961a5c9965336c6f8aeddea107e940ada675
Brian Simon <bsssshhhhhhh@gmail.com>
2022-07-23 21:52:28 -0400

    remove pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
   plugins: [
-    vue(),
-    VitePWA({
-      manifest: {
-        name: 'ABV Calculator',
-        background_color: '#FAE96F',
-        short_name: 'ABV',
-      },
-      injectRegister: 'inline',
-      registerType: 'autoUpdate'
-    })
+    vue()
   ]
 });
