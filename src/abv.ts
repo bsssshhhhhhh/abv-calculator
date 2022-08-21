c1e00dfacfafb426eb15c06f1ca3ff9c5537cd9b
Brian Simon <bsssshhhhhhh@gmail.com>
2022-08-21 05:30:49 +0000

    refactor

--- src/abv.ts
+++ src/abv.ts
@@ -15,20 +15,16 @@ export function useAbvCalculator({ originalGravity, finalGravity }: AbvCalculato
     const OG = originalGravity.value;
     const FG = finalGravity.value;
 
-    return <AbvCalculation[]>[
-      {
-        label: '131.25 * (OG - FG)',
-        calculatedAbvPercent: 131.25 * (OG - FG)
-      },
-      {
-        label: '(1.05 / 0.79) * (OG - FG) * 100',
-        calculatedAbvPercent: (1.05 / 0.79) * (OG - FG) * 100
-      },
-      {
-        label: '(76.08 * (OG - FG) / (1.775 - OG)) * (FG / 0.794)',
-        calculatedAbvPercent: (76.08 * (OG - FG) / (1.775 - OG)) * (FG / 0.794)
+    return [
+      '131.25 * (OG - FG)',
+      '(1.05 / 0.79) * (OG - FG) * 100',
+      '(76.08 * (OG - FG) / (1.775 - OG)) * (FG / 0.794)'
+    ].map((formula) => {
+      return {
+        label: formula,
+        calculatedAbvPercent: eval(`(OG, FG) => { return ${formula}; }`)(OG, FG)
       }
-    ];
+    });
   });
 
   return {
